a992b1adc1f6e00aec58e4bcab68429a37b5a369
tony-cannibal <castrovital.luis@gmail.com>
2023-11-07 11:00:00 -0600

    winbar update

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -9,12 +9,27 @@ return {
         "rcarriga/nvim-dap-ui",
     },
     config = function()
+        local conditions = require("heirline.conditions")
         local statusline = require("heirline.statusline.statusline")
+        local winbar = require("heirline.winbar")
         local bufferline = require("heirline.tabline.tabline")
+
+        local win_amount = #vim.api.nvim_tabpage_list_wins(0)
 
         require("heirline").setup({
             statusline = statusline,
-            tabline = bufferline
+            winbar = winbar,
+            tabline = bufferline,
+            opts = {
+                -- if the callback returns true, the winbar will be disabled for that window
+                -- the args parameter corresponds to the table argument passed to autocommand callbacks. :h nvim_lua_create_autocmd()
+                disable_winbar_cb = function(args)
+                    return conditions.buffer_matches({
+                        buftype = { "nofile", "prompt", "help", "quickfix" },
+                        filetype = { "^git.*", "fugitive", "Trouble", "dashboard" },
+                    }, args.buf)
+                end,
+            },
         })
     end
 }
